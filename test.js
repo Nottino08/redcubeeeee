24e575b4b44dedf4556923a379031f7a67dc53ae
Nottino08 <167352539+Nottino08@users.noreply.github.com>
2024-06-09 20:14:14 +0200

    Update test.js

--- test.js
+++ test.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', () => {
     setTimeout(() => {
-        window.location.href = 'index.html';
+        window.location.href = 'home.html';
     }, 5000);
 });
